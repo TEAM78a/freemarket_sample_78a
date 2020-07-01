c99b5cacd417f0cf48db37d1ada1f93ba906ae5d
Nagisa9029 <tzr.schicksal@gmail.com>
2020-07-01 11:59:03 +0900

    Add gem and Routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   root 'items#index'
+
+  resources :products do
+    get 'confirmation', on: :member
+  end
+    
+  resources :users, only: [:edit, :update, :show]
+
+  resources :card, only: [:show, :edit, :update]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
