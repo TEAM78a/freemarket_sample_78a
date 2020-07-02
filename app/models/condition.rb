584ccf773de8a5e627e1072f7f124fc1a6091470
naogo705 <66290412+naogo705@users.noreply.github.com>
2020-07-02 18:24:17 +0900

    クラス名のスペル修正

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,4 +1,4 @@
-class condition < ActiveHash::Base
+class Condition < ActiveHash::Base
   self.data = [
     {id:1, name: '新品/未使用'}, {id: 2, name:'未使用に近い'}, {id:3, name:'目立った傷や汚れなし'},
     {id:4, name: 'やや傷や汚れあり'}
