607ca28efeef5209d3517f66b170f84247cfbaea
naogo705 <66290412+naogo705@users.noreply.github.com>
2020-07-02 14:56:35 +0900

    productsテーブル作成

---
+++ db/migrate/20200702022310_create_products.rb
@@ -0,0 +1,20 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :name,              null: false
+      t.text :introduce,           null: false
+      t.integer :price,            null: false
+      t.string :kind,              null: false
+      t.string :brand,             null: false
+      t.string :condition,         null: false
+      t.string :postage,           null: false
+      t.string :shipment,          null: false
+      t.string :shipping_area,     null: false
+      t.boolean :sold_out_flg,     null: false, default: ""
+      t.references :user,          foreign_key: true
+      t.timestamps
+    end
+
+    add_index :products, :name
+  end
+end
