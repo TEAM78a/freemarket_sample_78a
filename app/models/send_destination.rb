c099c15a8b5f6e6f4b439c40321e4a99bcf5f475
ChriHem <64971017+ChriHem@users.noreply.github.com>
2020-07-02 18:14:21 +0900

    バリデーション設定（Send_destinationモデル）

--- app/models/send_destination.rb
+++ app/models/send_destination.rb
@@ -1,2 +1,13 @@
 class SendDestination < ApplicationRecord
+  belongs_to :user, optional: true
+  validates :family_name, 
+            :first_name, 
+            :family_name_kana, 
+            :first_name_kana, 
+            :postal_code, 
+            :prefectures, 
+            :municipalities,
+            :address,
+            :building,
+            :phone_number, presence: true
 end
