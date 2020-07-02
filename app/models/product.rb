a8242e09b2b4eb18339a84a16500b65de2db23bd
naogo705 <66290412+naogo705@users.noreply.github.com>
2020-07-02 15:00:42 +0900

    productモデル作成

---
+++ app/models/product.rb
@@ -0,0 +1,14 @@
+class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  has_many :images, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :favorites
+  has_many :userts, through: :favorites
+  belongs_to_active_hash :kind
+  belongs_to_active_hash :brand
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :shipment
+  belongs_to_active_hash :shipping_area
+end
