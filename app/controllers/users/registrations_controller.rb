e0eb950e3de1c4f3e2fd11d88996f816d8df0f3b
ChriHem <64971017+ChriHem@users.noreply.github.com>
2020-07-03 18:20:09 +0900

    createアクション編集。（セッションの設定）

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    @user = User.new(sign_up_params)
+    unless @user.valid?
+      flash.now[:alert] = @user.errors.full_messages
+      render :new and return
+    end
+    session["devise.regist_data"] = {user: @user.attributes}
+    session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    @send_destination = @user.build_send_destinaton
+    render :new_send_destination
+  end
 
   # GET /resource/edit
   # def edit
