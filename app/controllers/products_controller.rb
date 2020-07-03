14863eaef0be31860c05429671612d6782b5c9c7
naogo705 <66290412+naogo705@users.noreply.github.com>
2020-07-03 11:23:55 +0900

    モデル、コントローラー修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,10 +5,20 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
-    @product.images.new
+    # @product.images.new
+    @product.images.build
   end
 
   def create
+    @product = Product.new(product_params)
+    binding.pry
+    if @product.save
+      # binding.pry
+      redirect_to root_path
+    else
+      # binding.pry
+      render :new
+    end
   end
 
   def show
@@ -23,4 +33,23 @@ class ProductsController < ApplicationController
   def destroy
   end
 
+  private
+
+  def product_params
+    params.require(:product).permit(:name,
+                                    :introduce,
+                                    :price,
+                                    :kind,
+                                    :brand,
+                                    :condition,
+                                    :postage,
+                                    :shipment,
+                                    :shipping_area,
+                                    images_attributes:[
+                                      :id,
+                                      :product_id,
+                                      :image,
+                                      :_destroy
+                                    ])
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,16 +1,16 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :user
+  # belongs_to :user
   has_many :images, dependent: :destroy
-  has_many :comments, dependent: :destroy
-  has_many :favorites
-  has_many :userts, through: :favorites
-  belongs_to_active_hash :kind
-  belongs_to_active_hash :brand
-  belongs_to_active_hash :condition
-  belongs_to_active_hash :postage
-  belongs_to_active_hash :shipment
-  belongs_to_active_hash :shipping_area
+  # has_many :comments, dependent: :destroy
+  # has_many :favorites
+  # has_many :users, through: :favorites
+  # belongs_to_active_hash :kind
+  # belongs_to_active_hash :brand
+  # belongs_to_active_hash :condition
+  # belongs_to_active_hash :postage
+  # belongs_to_active_hash :shipment
+  # belongs_to_active_hash :shipping_area
 
   accepts_nested_attributes_for :images, allow_destroy: true
 end
