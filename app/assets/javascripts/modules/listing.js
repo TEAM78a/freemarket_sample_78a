6c4a569ec90857f5eefee8bb410711b8e77109e9
Nagisa9029 <tzr.schicksal@gmail.com>
2020-07-06 01:40:16 +0900

    Add js listing

---
+++ app/assets/javascripts/modules/listing.js
@@ -0,0 +1,10 @@
+$(function() {
+  let tabs = $(".menu_item");
+  function tabSwitch() {
+    $(".active").removeClass("active");
+    $(this).addClass("active");
+    const index = tabs.index(this);
+    $(".Content").removeClass("show").eq(index).addClass("show");
+  }
+  tabs.click(tabSwitch);
+});
