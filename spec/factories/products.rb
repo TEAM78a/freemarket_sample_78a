daf10cd7079ff13892bbf9fdcd00f1ed762ca50a
naogo705 <66290412+naogo705@users.noreply.github.com>
2020-07-06 15:02:31 +0900

    商品出品機能テストコードまとめ

---
+++ spec/factories/products.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :product do
+    id                             {"1"}
+    name                           {"犬と猿"}
+    introduce                      {"動物のイラスト"}
+    price                          {"30"}
+    kind_id                        {"1"}
+    brand_id                       {"1"}
+    condition_id                   {"1"}
+    postage_id                     {"1"}
+    shipment_id                    {"1"}
+    prefecture_id                  {"1"}
+    after(:build) do |product|
+      product.images << build_list(:image, 2, product: product)
+    end
+  end
+end
