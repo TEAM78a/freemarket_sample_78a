551f7e3f5c0dc016ba8b46b9aaa80a3af6f2896a
ChriHem <64971017+ChriHem@users.noreply.github.com>
2020-07-06 16:45:37 +0900

    usersテーブル、バリデーション

--- app/models/send_destination.rb
+++ app/models/send_destination.rb
@@ -1,13 +1,5 @@
 class SendDestination < ApplicationRecord
   belongs_to :user, optional: true
-  validates :family_name, 
-            :first_name, 
-            :family_name_kana, 
-            :first_name_kana, 
-            :postal_code, 
-            :prefectures, 
-            :municipalities,
-            :address,
-            :building,
-            :phone_number, presence: true
+  validates :family_name, :first_name, :family_name_kana, :first_name_kana, 
+            :postal_code, :prefectures, :municipalities, :address, :phone_number, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday ,presence: true
+  validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana, :first_name_kana, format: { with: /\A[ぁ-んー－]+\z/ }
+  validates :email, uniqueness: true
+
   has_one :send_destination
 end
