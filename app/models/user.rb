a60f0855fb59548f91beb897a8d4958d9f59f358
ChriHem <64971017+ChriHem@users.noreply.github.com>
2020-07-07 12:30:47 +0900

    バリデーションupdate

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday ,presence: true
-  validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
   validates :family_name_kana, :first_name_kana, format: { with: /\A[ぁ-んー－]+\z/ }
   validates :email, uniqueness: true
 
