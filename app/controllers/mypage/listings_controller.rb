292a535fb99e007fdc67a2e11fa0ecdfedcea87c
Nagisa9029 <tzr.schicksal@gmail.com>
2020-07-10 15:44:46 +0900

    部分テンプレート修正

--- app/controllers/mypage/listings_controller.rb
+++ app/controllers/mypage/listings_controller.rb
@@ -2,6 +2,7 @@ class Mypage::ListingsController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    
+    @products_sale = Product.where(user_id: current_user.id).where(sold_out_flg: 0)
+    @products_soldout = Product.where(user_id: current_user.id).where(sold_out_flg: 1)
   end
 end
