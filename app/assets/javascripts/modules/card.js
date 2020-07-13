c7cf5fb76ac71c168a720c6fc04311db58aecde8
Nagisa9029 <tzr.schicksal@gmail.com>
2020-07-13 17:30:41 +0900

    トークン生成（jsファイル）作成

--- app/assets/javascripts/modules/card.js
+++ app/assets/javascripts/modules/card.js
@@ -1,17 +1,31 @@
 $(function(){
-  Payjp.setPublicKey('Rails.application.credentials[:payjp][:PUBLIC_KEY]');
+  Payjp.setPublicKey('pk_test_90588d9619d4cd0ae1005186');
     
     $('#token_submit').click(function(e){
-    // console.log('Rails.application.credentials[:payjp][:PUBLIC_KEY]');
     e.preventDefault();
 
     let card = {
-        number: document.getElementById("payment_card_no").value,
-        cvc: document.getElementById("payment_card_cvc").value,
-        exp_month: document.getElementById("payment_card_month").value,
-        exp_year: document.getElementById("payment_card_year").value
+        number: $('#payment_card_no').val(),
+        cvc: $('#payment_card_cvc').val(),
+        exp_month: $('#payment_card_month').val(),
+        exp_year: $('#payment_card_year').val()
     };
-
     console.log(card);
+    Payjp.createToken(card, function(status, response) {
+      if (status === 200) {
+        $('#payment_card_no').removeAttr("name");
+        $('#payment_card_cvc').removeAttr("name");
+        $('#payment_card_month').removeAttr("name");
+        $('#payment_card_year').removeAttr("name"); 
+        $('#card_token').append(
+          $('<input type="hidden" name="payjp_token">').val(response.id)
+        );
+        document.inputForm.submit();
+        // alert("登録が完了しました");
+      } else {
+        // redirect_to new_mypage_card_path
+        // alert("カード情報が正しくありません。");
+      }
+    });
   });
 });
