0f015e53a75d1c6b073f44efdd2d9b89e48d5264
naogo705 <66290412+naogo705@users.noreply.github.com>
2020-07-15 18:10:31 +0900

    売り切れ商品のみの売り切れ表示実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,8 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:mypage, :logout]
 
   def index
+    @products = Product.all.order("created_at DESC").limit(4)
+    @products_random = Product.order("RAND()").limit(4)
   end
 
   def mypage
