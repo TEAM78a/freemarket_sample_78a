9cbac5b057dcf8daad371f4b2e722cc80cbd3c94
ChriHem <64971017+ChriHem@users.noreply.github.com>
2020-07-15 14:38:36 +0900

    SnsCredentialモデルの作成。provider, uid, user_id外部キーのテーブルを作成。
    rails g model sns_credential provider:string uid:string user:references
    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_10_035751) do
+ActiveRecord::Schema.define(version: 2020_07_15_053305) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "image", null: false
@@ -55,6 +55,15 @@ ActiveRecord::Schema.define(version: 2020_07_10_035751) do
     t.index ["user_id"], name: "index_send_destinations_on_user_id"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "family_name", null: false
@@ -75,4 +84,5 @@ ActiveRecord::Schema.define(version: 2020_07_10_035751) do
 
   add_foreign_key "images", "products"
   add_foreign_key "products", "users"
+  add_foreign_key "sns_credentials", "users"
 end
