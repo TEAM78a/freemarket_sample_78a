99a8d05c263e3b1c1f8c0de375e0feb09f510b82
ChriHem <64971017+ChriHem@users.noreply.github.com>
2020-07-15 15:11:21 +0900

    アソシエーションの追加（Userモデル、SnsCredentialモデル）

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+         
   validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday ,presence: true
   validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
   validates :family_name_kana, :first_name_kana, format: { with: /\A[ぁ-んー－]+\z/ }
   validates_uniqueness_of :email, case_sensitive: false
-
+  
   has_one :send_destination
   has_many :products
+  has_many :sns_credentials
 end
