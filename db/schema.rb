03fc4cd7608a9b2f93f16b17eeb8b8c00f042d78
ChriHem <64971017+ChriHem@users.noreply.github.com>
2020-07-16 12:42:56 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_13_013613) do
+ActiveRecord::Schema.define(version: 2020_07_15_053305) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -65,6 +65,15 @@ ActiveRecord::Schema.define(version: 2020_07_13_013613) do
     t.index ["user_id"], name: "index_send_destinations_on_user_id"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "family_name", null: false
@@ -86,4 +95,5 @@ ActiveRecord::Schema.define(version: 2020_07_13_013613) do
   add_foreign_key "cards", "users"
   add_foreign_key "images", "products"
   add_foreign_key "products", "users"
+  add_foreign_key "sns_credentials", "users"
 end
