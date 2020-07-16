c32c038a1f0773b02bf58ad8bc39350866f10dbf
ChriHem <64971017+ChriHem@users.noreply.github.com>
2020-07-16 17:41:10 +0900

    コントローラー編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(params.require(:comment).permit(:text).merge(user_id: current_user.id, product_id: params[:product_id]))
+    comment = Comment.create(comment_params)
     redirect_to product_path
   end
 
   private
 
   def comment_params
-    
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, product_id: params[:product_id])
   end
 end
