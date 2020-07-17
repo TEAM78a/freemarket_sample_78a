e7ca9ce2bcd2c574cb37d5b8163d4be8019c854e
Nagisa9029 <tzr.schicksal@gmail.com>
2020-07-17 11:51:09 +0900

    コントローラー設定

--- app/controllers/mypage/destinations_controller.rb
+++ app/controllers/mypage/destinations_controller.rb
@@ -1,11 +1,32 @@
 class Mypage::DestinationsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_destination
+  before_action :edit_validate
   
   def edit
-    
   end
 
   def update
-    
+    binding.pry
+    if @destination.update(destination_params)
+      redirect_to mypage_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def destination_params
+    params.require(:send_destination).permit(:nickname, :email)
+  end
+
+  def set_destination
+    @destination = SendDestination.find(params[:id])
+  end
+
+  def edit_validate
+    if @destinationuser_id != current_user.id
+      redirect_to root_path
+    end
   end
 end
