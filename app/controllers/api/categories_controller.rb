b8d6c1e234b3c2381b77ce8b0806b823486f1b58
Nagisa9029 <tzr.schicksal@gmail.com>
2020-07-19 21:40:43 +0900

    Add Search機能（json）

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -7,6 +7,20 @@ class Api::CategoriesController < ApplicationController
     end
 
     respond_to do |format|
+      format.html{
+        if params[:id] == "0"
+          redirect_to products_path
+        else
+          search_params = Kind.find(params[:id]).children.ids
+          Kind.find(params[:id]).children.ids.each do |children|
+            Kind.find(children).children.ids.each do |grand|
+              search_params << grand
+            end
+          end
+          search_params << params[:id].to_i
+          redirect_to products_path({q: {"kind_id_in": search_params, commit: "Search" }})
+        end
+      }
       format.json{
         render json: @categories
       }
