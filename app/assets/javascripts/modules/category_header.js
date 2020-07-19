e83b45a4825b7731e72145a6727954493ad2637b
Nagisa9029 <tzr.schicksal@gmail.com>
2020-07-19 22:46:32 +0900

    Fix Brand#Search

--- app/assets/javascripts/modules/category_header.js
+++ app/assets/javascripts/modules/category_header.js
@@ -1,6 +1,6 @@
 $(function(){
   function buildHTML(data){
-    var html = $("<div>").addClass("Headfield__Bottom__left__nav__field--menu")
+    var html = $("<div>").addClass("Headfield__Bottom__left__category__field--menu")
     var link
     data.forEach(function(value){
       link = $("<a>", {
@@ -14,11 +14,11 @@ $(function(){
   }
 
   $("#catroy_top_title").mouseenter(function(){
-    $(".Headfield__Bottom__left__nav__field--menu").remove()
+    $(".Headfield__Bottom__left__category__field--menu").remove()
   })
 
-  $(".Headfield__Bottom__left__nav").mouseleave(function(){
-    $(".Headfield__Bottom__left__nav__field--menu").remove()
+  $(".Headfield__Bottom__left__category").mouseleave(function(){
+    $(".Headfield__Bottom__left__category__field--menu").remove()
   })
 
   $(document).on({
@@ -35,10 +35,19 @@ $(function(){
 
         if( result.length != 0 ){
           var html = buildHTML(result)
-          $(this).parent().parent().nextAll(".Headfield__Bottom__left__nav__field--menu").remove()
-          $(".Headfield__Bottom__left__nav__field").append(html)
+          $(this).parent().parent().nextAll(".Headfield__Bottom__left__category__field--menu").remove()
+          $(".Headfield__Bottom__left__category__field").append(html)
         } 
       })
+
+      $(".Headfield__Bottom__left__category__field--menu").mouseenter(function(eo){ 
+        $(this).find("a").css("color","")
+      })
+    },'mouseleave' : function(){
+
+      $(".Headfield__Bottom__left__category__field--menu").mouseleave(function(eo){
+        $(eo.fromElement).css("color","orange")
+      })
     }
-  }) 
+  }, ".category_name") 
 })
