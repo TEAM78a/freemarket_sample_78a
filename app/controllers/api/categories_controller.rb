2d19b057013c3a4c95d85c888835fec15cd4a362
Nagisa9029 <tzr.schicksal@gmail.com>
2020-07-19 19:49:25 +0900

    Add json ancestry(nil)

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,2 +1,11 @@
 class Api::CategoriesController < ApplicationController
+  def show
+    @categories = Kind.where(ancestry:nil)
+
+    respond_to do |format|
+      format.json{
+        render json: @categories
+      }
+    end 
+  end
 end
