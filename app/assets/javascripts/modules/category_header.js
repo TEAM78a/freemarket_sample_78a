8e94df210d57a0b390ee72c0d62298d48c53598e
Nagisa9029 <tzr.schicksal@gmail.com>
2020-07-19 19:50:48 +0900

    Add js/html生成

--- app/assets/javascripts/modules/category_header.js
+++ app/assets/javascripts/modules/category_header.js
@@ -1,4 +1,17 @@
 $(function(){
+  function buildHTML(data){
+    var html = $("<div>").addClass("Headfield__Bottom__left__nav__field--menu")
+    var link
+    data.forEach(function(value){
+      link = $("<a>", {
+        href: "/api/categories/" + value.id ,
+        "class":"category_name no-underline"
+      }).text(value.name)
+      link = $("<p>").append(link)
+      html.append(link)
+    })
+    return html
+  }
 
   $(document).on({
     'mouseenter' : function() {
@@ -9,6 +22,14 @@ $(function(){
         dataType: "json",
         context:this,
         cache: false,
+      })
+      .done(function(result){
+
+        if( result.length != 0 ){
+          var html = buildHTML(result)
+          $(this).parent().parent().nextAll(".Headfield__Bottom__left__nav__field--menu").remove()
+          $(".Headfield__Bottom__left__nav__field").append(html)
+        } 
       })
     }
   }) 
