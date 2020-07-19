3265dfe693836a1a0f0b86a72e200d2e1cf11ebb
Nagisa9029 <tzr.schicksal@gmail.com>
2020-07-19 19:48:00 +0900

    Add js/json

--- app/assets/javascripts/modules/category_header.js
+++ app/assets/javascripts/modules/category_header.js
@@ -1,8 +1,15 @@
 $(function(){
+
   $(document).on({
     'mouseenter' : function() {
       var path = $(this).attr("href");
- 
+      $.ajax({
+        url: path,
+        type: "GET",
+        dataType: "json",
+        context:this,
+        cache: false,
+      })
     }
   }) 
 })
