09d05e7e04280bcc958dc4f73316cf8d4e332ab2
Nagisa9029 <tzr.schicksal@gmail.com>
2020-07-19 19:57:49 +0900

    Fix json/redirect_to(kind.ancestry[nil])

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -7,6 +7,11 @@ class Api::CategoriesController < ApplicationController
     end
 
     respond_to do |format|
+      format.html{
+        if params[:id] == "0"
+          redirect_to products_path
+        end
+      }
       format.json{
         render json: @categories
       }
