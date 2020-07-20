de2bc0dd596320edc3e9982e156cc984e09c0918
ChriHem <64971017+ChriHem@users.noreply.github.com>
2020-07-20 18:32:05 +0900

    console.log削除

--- app/assets/javascripts/modules/comment.js
+++ app/assets/javascripts/modules/comment.js
@@ -12,9 +12,7 @@ $(function(){
 
   $('#new_comment').on('submit', function(e){
     e.preventDefault();
-    // console.log(this)
     var formData = new FormData(this);
-    // console.log(formData)
     var url = $(this).attr('action')
     $.ajax({
       url: url,
