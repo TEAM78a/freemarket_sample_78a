41127e8b5c2fae3f0eeb1eff35833fc659f89728
naogo705 <66290412+naogo705@users.noreply.github.com>
2020-07-20 17:38:31 +0900

    Update favorites_controller.rb

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,16 @@
 class FavoritesController < ApplicationController
 
-
   def create
-    # binding.pry
-    @product = Product.find(params[:product_id])
-    @product.like(current_user)
-    
-                            # (
-                            #   user_id: current_user.id, 
-                            #   product_id: @product.id
-                            # )
+    @favorite = Favorite.new(
+                              user_id: current_user.id, 
+                              product_id: params[:product_id]
+                            )
 
-    # if @favorite.save
-    #   redirect_to mypage_listings_path
-    # end
+                            binding.pry
+
+    @favorite.save
+    redirect_to mypage_listings_path
+    
   end
 
   def destroy
