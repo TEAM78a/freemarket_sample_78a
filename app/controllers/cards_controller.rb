3aae308ec6ec00398baeaa3abdf7cecddfbbd32a
Nagisa9029 <tzr.schicksal@gmail.com>
2020-07-22 12:07:33 +0900

    コントローラー設定

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,10 +1,34 @@
 class CardsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_api_key, only:[:index]
+  before_action :set_cards
+  before_action :set_card, only:[:update]
 
   def index
-    
+    @product_id = params[:id]
   end
 
   def update
-    
+    card = @cards.find_by(default_flg: 1)
+    card.default_flg = 0
+    @card.default_flg = 1
+    if card.save && @card.save
+      redirect_to purchase_products_path(params[:product_id])
+    else
+      render :index
+    end
+  end
+
+  private
+  def set_api_key
+    Payjp.api_key = Rails.application.credentials[:payjp][:ACCESS_KEY]
+  end
+
+  def set_cards
+    @cards= current_user.cards
+  end
+
+  def set_card
+    @card = Card.find(params[:id])
   end
 end
--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,10 +1,41 @@
 class DestinationsController < ApplicationController
-
+  before_action :authenticate_user!
+  before_action :set_destination
+  before_action :edit_validate
+  
   def edit
-    
   end
 
   def update
-    
+    if @destination.update(destination_params)
+      redirect_to purchase_products_path(params[:product_id])
+    else
+      render :edit
+    end
+  end
+
+  private
+  def destination_params
+    params.require(:send_destination).permit(:family_name,
+                                             :first_name,
+                                             :family_name_kana,
+                                             :first_name_kana,
+                                             :postal_code,
+                                             :prefectures,
+                                             :municipalities,
+                                             :address,
+                                             :building,
+                                             :phone_number
+                                             )
+  end
+
+  def set_destination
+    @destination = SendDestination.find(params[:id])
+  end
+
+  def edit_validate
+    if @destination.user_id != current_user.id
+      redirect_to root_path
+    end
   end
 end
